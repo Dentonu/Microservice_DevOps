6fba4a48a7f775e226133275e1cfb1f73662070f
Ubuntu <ubuntu@ip-172-31-86-55.ec2.internal>
2022-08-12 14:40:07 +0000

    Added project files

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=danlenchy/project:v8
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag  danlenchy/project:v8  danlenchy/project:v9
+docker login -u danlenchy -p $PASSWORD
 # Step 3:
 # Push image to a docker repository
+docker push danlenchy/project:v8
